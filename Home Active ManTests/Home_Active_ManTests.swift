c4b871b50b431eeb1246bcba77e62d5ef2c69541
Josef Santamaria <josef.santamaria@gmail.com>
2020-11-25 19:39:43 +0100

    request data from url on load

--- Home Active ManTests/Home_Active_ManTests.swift
+++ Home Active ManTests/Home_Active_ManTests.swift
@@ -8,20 +8,35 @@
 import XCTest
 
 class RemoteWorkoutLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class Home_Active_ManTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let _ = RemoteWorkoutLoader()
+        let client = HTTPClient.shared
+        _ = RemoteWorkoutLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_init_loadDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteWorkoutLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
