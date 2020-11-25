5ec4e8a6823811c02bf8ba7aaa8c44ab377b0419
Josef Santamaria <josef.santamaria@gmail.com>
2020-11-25 20:46:12 +0100

    Refactor test to remove duplication

--- Home Active ManTests/Home_Active_ManTests.swift
+++ Home Active ManTests/Home_Active_ManTests.swift
@@ -25,32 +25,40 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-        
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
 
 class Home_Active_ManTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://a-url.com")!
-        _ = RemoteWorkoutLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_init_loadDataFromURL() {
-        let client = HTTPClientSpy()
         let url = URL(string: "https://a-given-url.com")!
-        let sut = RemoteWorkoutLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+
+    //MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteWorkoutLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteWorkoutLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+            
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 
 }
