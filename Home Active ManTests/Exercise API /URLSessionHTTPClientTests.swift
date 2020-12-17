ed205c158dd3e19ea2bc480f6b3ab892ab448906
Josef Santamaria <josef.santamaria@gmail.com>
2020-12-17 23:04:09 +0100

    Move ´HTTPURLSession´ to its own file in production

--- Home Active ManTests/Exercise API /URLSessionHTTPClientTests.swift
+++ Home Active ManTests/Exercise API /URLSessionHTTPClientTests.swift
@@ -8,27 +8,6 @@
 import XCTest
 import Home_Active_Man
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValuesrepresentation: Error {}
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            }else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            }else{
-                completion(.failure(UnexpectedValuesrepresentation()))
-            }
-        }.resume()
-    }
-}
 
 class URLSessionHTTPClientTests: XCTestCase {
     
