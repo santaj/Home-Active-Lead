8ef7203989cb058871f403139dc67bba66b05d9b
Josef Santamaria <josef.santamaria@gmail.com>
2020-12-17 22:59:16 +0100

    Make HTTPURLClient to conform to HTTPClient protocol

--- Home Active ManTests/Exercise API /URLSessionHTTPClientTests.swift
+++ Home Active ManTests/Exercise API /URLSessionHTTPClientTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import Home_Active_Man
 
-class URLSessionHTTPClient {
+class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     init(session: URLSession = .shared) {
@@ -98,7 +98,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return URLSessionHTTPClient()
