aa6e1143306eca77de45c4518e1343dfe7d0ff18
Josef Santamaria <josef.santamaria@gmail.com>
2020-12-21 10:29:42 +0100

    Add memory leak component into endtoend target

--- Home_Active_ManAPIEndToEndTests/Home_Active_ManAPIEndToEndTests.swift
+++ Home_Active_ManAPIEndToEndTests/Home_Active_ManAPIEndToEndTests.swift
@@ -33,10 +33,12 @@ class Home_Active_ManAPIEndToEndTests: XCTestCase {
     
     //MARK: - Helpers
     
-    private func getFeedResult() -> LoadExerciseResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadExerciseResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteExerciseLoader(url: testServerURL, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader)
         
         let exp = expectation(description: "Wait for load completion")
         
